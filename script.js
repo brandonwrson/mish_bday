b690ad0c4a94d019dfa617badc838193a7aabe62
brandonwrson <brandonwrson@gmail.com>
2024-02-20 22:13:22 -0800

    changes

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("clickMeButton").addEventListener("click", function() {
+    var flowers = document.getElementById("flowers");
+    var birthdaySign = document.getElementById("birthdaySign");
+    flowers.classList.toggle("hidden");
+    birthdaySign.classList.toggle("hidden");
+    // You can add more sophisticated animations or actions here
+  });
+  
